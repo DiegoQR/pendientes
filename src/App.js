a76fa9133d5147a8b6560725231146cc17037ca1
Diego Quinones <diegoqr2000@gmail.com>
2021-09-09 09:04:16 -0400

    Se clasifica las tareas por categoria

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 const todoName = document.querySelector("#todo-name-input");
 const form = document.querySelector("#saludador-form");
+const type = document.querySelector("#type-input");
 
-const listOutput = document.querySelector("#list-output")
+const listOutput = document.querySelector("#list-output");
+const listTypeOutput = document.querySelector("#list-type-output");
 
-todoList = []
-
+todoList = [];
+typeList = [];
 
 
 form.addEventListener("submit", event => {
     todoList.push(todoName.value);
-    listOutput.innerHTML = todoList[0];
-    //todoList.forEach(function(item) {
-        
-    //})
-    
+    typeList.push(type.value);
+    listOutput.innerHTML = todoList;
+    listOutput.innerHTML = typeList;
 })
